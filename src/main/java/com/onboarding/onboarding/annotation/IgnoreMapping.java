fd7ecf728ac7d0e03ccb298506331377aad8820e
HelloWorld-n <gajica.b@gmail.com>
2022-10-15 19:54:25 +0200

    remained:   src/main/java/com/onboarding/onboarding/annotation/IgnoreMapping.java

--- src/main/java/com/onboarding/onboarding/annotation/IgnoreMapping.java
+++ src/main/java/com/onboarding/onboarding/annotation/IgnoreMapping.java
@@ -1,5 +1,5 @@
 package com.onboarding.onboarding.annotation;
-;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
@@ -8,5 +8,5 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface IgnoreMapping {
-	
+
 }
